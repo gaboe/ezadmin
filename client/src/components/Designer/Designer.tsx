71f8647feda52c7306fa529588168c945be5ac30
gaboe <gabrielecegi@gmail.com>
2018-09-29 11:20:43 +0200

    Add DbTableDetail

--- client/src/components/Designer/Designer.tsx
+++ client/src/components/Designer/Designer.tsx
@@ -1,71 +1,16 @@
 import * as React from "react";
-import { Col, Row } from "react-grid-system";
-import { Link, RouteComponentProps } from "react-router-dom";
-import { Button, Checkbox, Header, List } from "semantic-ui-react";
-import { GetDbTableDetailQueryVariables } from "../../generated-types/types";
-import {
-  DB_TABLE_DETAIL_QUERY,
-  DbTablesDetailQueryComponent
-} from "../../graphql/queries/DbExplorer/TableDetail";
+import { RouteComponentProps } from "react-router-dom";
+import { DbTableDetail } from "./DbTableDetail/DbTableDetail";
+
 type Props = RouteComponentProps<{ name: string }>;
 type State = {};
 class Designer extends React.Component<Props, State> {
   public render() {
     const { name } = this.props.match.params;
-    const variables: GetDbTableDetailQueryVariables = { tableName: name };
 
     return (
       <>
-        <Header as="h1">{variables.tableName}</Header>
-        <DbTablesDetailQueryComponent
-          query={DB_TABLE_DETAIL_QUERY}
-          variables={variables}
-        >
-          {response => {
-            if (response.loading || !response.data || !response.data.table) {
-              return (
-                <>
-                  <p>Loading...</p>
-                </>
-              );
-            }
-
-            return (
-              <>
-                <Row>
-                  <Col md={6} lg={3}>
-                    <Header as="h4">
-                      Schema: {response.data.table.schemaName}
-                    </Header>
-                    <Link
-                      to={`/${response.data.table.schemaName}-${
-                        response.data.table.tableName
-                      }`}
-                    >
-                      <Button content="Back" />
-                    </Link>
-
-                    <Header as="h5">Columns:</Header>
-                    <List size="large" divided={true} celled={true}>
-                      {response.data.table.columns.map(x => {
-                        return (
-                          <List.Item key={x.columnName}>
-                            <Checkbox
-                            // onClick={() =>
-                            //   this.checkColumnFromPrimaryTable(x, true)
-                            // }
-                            />
-                            {` [${x.columnName}]: ${x.dataType.toLowerCase()}`}
-                          </List.Item>
-                        );
-                      })}
-                    </List>
-                  </Col>
-                </Row>
-              </>
-            );
-          }}
-        </DbTablesDetailQueryComponent>
+        <DbTableDetail variables={{ tableName: name }} />
       </>
     );
   }
