0bf85ae16563ce1dd17f16eda97dbd864de999f2
gaboe <gabrielecegi@gmail.com>
2018-09-30 22:58:26 +0200

    Refactor GraphQL types

--- server/src/BLogic.EzAdmin.GraphQL/GraphQLSchema.fs
+++ server/src/BLogic.EzAdmin.GraphQL/GraphQLSchema.fs
@@ -1,5 +1,4 @@
 ﻿namespace BLogic.EzAdmin.GraphQL
-#nowarn "40"
 
 type Root =
     { ClientId: string }
@@ -11,162 +10,12 @@ module GraphQLSchema =
     open BLogic.EzAdmin.Domain.SqlTypes
     open BLogic.EzAdmin.Domain.UiTypes
     open BLogic.EzAdmin.Core.Services.SqlTypes.SqlTypeService
-    open BLogic.EzAdmin.Domain.GraphQL.InputTypes
-
-    type SqlColumnDataType = Int | Nvarchar | Unknown
-        
-    let getSqlColumnDataType sqlDataType = match sqlDataType with 
-                                            | "int" -> Int 
-                                            | "nvarchar" -> Nvarchar 
-                                            | _ -> Unknown
-
-    let SqlColumnDataType =
-        Define.Enum(
-            name = "SqlColumnDataType",
-            options = [
-                Define.EnumValue("Int", SqlColumnDataType.Int, "")
-                Define.EnumValue("Char", SqlColumnDataType.Nvarchar, "")
-                Define.EnumValue("Unknown", SqlColumnDataType.Unknown, "")
-             ])
+    open BLogic.EzAdmin.GraphQL.InputGraphQLTypes
+    open BLogic.EzAdmin.GraphQL.QueryGraphQLTypes
 
     let schemaConfig = SchemaConfig.Default
-
-    let rec SqlSchemaType =
-       Define.Object<SqlSchema>(
-            name = "SqlSchema",
-            description = "Db schema",
-            isTypeOf = (fun o -> o :? SqlSchema),
-            fieldsFn = fun () ->
-            [
-                Define.Field("schemaName", String, "Schema name", fun _ (x: SqlSchema) -> x.SchemaName)
-            ]
-        )
-
-    and SqlReferenceType = 
-        Define.Object<SqlReference>(
-            name = "SqlReference",
-            description = "Relation between table reference constrains",
-            isTypeOf = (fun o -> o :? SqlReference),
-            fieldsFn = fun () ->
-            [
-                Define.Field("referenceName", String, "Name of reference", fun _ (x: SqlReference) -> x.ReferenceName)
-                Define.Field("fromSchema", String, "", fun _ (x: SqlReference) -> x.FromSchema)
-                Define.Field("fromTable", String, "", fun _ (x: SqlReference) -> x.FromTable)
-                Define.Field("fromColumn", String, "", fun _ (x: SqlReference) -> x.FromColumn)
-                Define.Field("toSchema", String, "", fun _ (x: SqlReference) -> x.ToSchema)
-                Define.Field("toTable", String, "", fun _ (x: SqlReference) -> x.ToTable)
-                Define.Field("toColumn", String, "", fun _ (x: SqlReference) -> x.ToColumn)
-            ]
-         )
-
-    and SqlTableType = 
-        Define.Object<SqlTable>(
-            name = "SqlTable",
-            description = "",
-            isTypeOf = (fun o -> o :? SqlTable),
-            fieldsFn = fun () ->
-            [
-                Define.Field("tableName", String, "Table name", fun _ (x: SqlTable) -> x.TableName)
-                Define.Field("schemaName", String, "Schema name", fun _ (x: SqlTable) -> x.SchemaName)
-                Define.Field("columns", ListOf (SqlColumnType), "Columns of table", fun _ (x: SqlTable) -> getColumns x.TableName |> Async.RunSynchronously)
-                Define.Field("referencesToTable", ListOf (SqlReferenceType), "Column references to this table", fun _ (x: SqlTable) -> getReferencesToTable x.TableName |> Async.RunSynchronously)
-                Define.Field("referencesFromTable", ListOf (SqlReferenceType), "Column references from this table to other tables", fun _ (x: SqlTable) -> getReferencesFromTable x.TableName |> Async.RunSynchronously)
-            ]
-        )
-
-    and SqlColumnType = 
-        Define.Object<SqlColumn>(
-            name = "SqlColumn",
-            description = "",
-            isTypeOf = (fun o -> o :? SqlColumn),
-            fieldsFn = fun () ->
-            [
-                Define.Field("columnName", String, "Column name", fun _ (x: SqlColumn) -> x.ColumnName)
-                Define.Field("tableName", String, "Table name", fun _ (x: SqlColumn) -> x.TableName)
-                Define.Field("schemaName", String, "Schema name", fun _ (x: SqlColumn) -> x.SchemaName)
-                Define.Field("dataType", SqlColumnDataType, "", fun _ (h : SqlColumn) -> h.DataType |> getSqlColumnDataType )
-            ]
-        )
     
-    and ColumnType = 
-        Define.Object<Column>(
-            name = "Column",
-            description = "",
-            isTypeOf = (fun o -> o :? Column),
-            fieldsFn = fun () ->
-            [
-                Define.Field("name", String, "", fun _ (x: Column) -> x.Name)
-                Define.Field("value", String, "", fun _ (x: Column) -> x.Value)
-            ]
-        )
-
-    and RowType = 
-        Define.Object<Row>(
-            name = "Row",
-            description = "",
-            isTypeOf = (fun o -> o :? Row),
-            fieldsFn = fun () ->
-            [
-                Define.Field("key", String, "Represents unique key of row", fun _ (x: Row) -> x.Key)
-                Define.Field("columns", ListOf (ColumnType), "Multiple properties of record", fun _ (x: Row) -> x.Columns)
-            ]
-        )
-    
-    and TableType = 
-        Define.Object<Table>(
-            name = "Table",
-            description = "",
-            isTypeOf = (fun o -> o :? Table),
-            fieldsFn = fun () ->
-            [
-                Define.Field("rows", ListOf (RowType), "Rows in talbe", fun _ (x: Table) -> x.Rows)
-            ]
-        )
-
-    and PageType = 
-        Define.Object<Page>(
-            name = "Page",
-            description = "",
-            isTypeOf = (fun o -> o :? Page),
-            fieldsFn = fun () ->
-            [
-                Define.Field("table", TableType, "Table on page", fun _ (x: Page) -> x.Table)
-            ]
-        )
-    and MenuItemType = 
-        Define.Object<MenuItem>(
-            name = "MenuItem",
-            description = "",
-            isTypeOf = (fun o -> o :? MenuItem),
-            fieldsFn = fun () ->
-            [
-                Define.Field("name", String, "Table on page", fun _ (x: MenuItem) -> x.Name)
-                Define.Field("rank", SchemaDefinitions.Int, "Table on page", fun _ (x: MenuItem) -> x.Rank)
-            ]
-        )
-    and AppType = 
-        Define.Object<App>(
-            name = "App",
-            description = "",
-            isTypeOf = (fun o -> o :? App),
-            fieldsFn = fun () ->
-            [
-                Define.Field("pages", ListOf (PageType), "Pages in app", fun _ (x: App) -> x.Pages)
-                Define.Field("menuItems", ListOf (MenuItemType), "Menu items", fun _ (x: App) -> x.MenuItems)
-            ]
-        )
-
-     and AppPreviewInputType = 
-        Define.InputObject<AppInputType>(
-            name = "AppPreviewInput",
-            fieldsFn = fun () ->
-            [
-                Define.Input("schemaName", String)
-                Define.Input("tableName", String)
-            ]
-        )
-
-    and RootType =
+    let RootType =
         Define.Object<Root>(
             name = "Root",
             description = "The Root type to be passed to all our resolvers",
---
+++ server/src/BLogic.EzAdmin.GraphQL/InputGraphQLTypes.fs
@@ -0,0 +1,17 @@
+﻿namespace BLogic.EzAdmin.GraphQL
+
+open FSharp.Data.GraphQL.Types
+
+open BLogic.EzAdmin.Domain.GraphQL.InputTypes
+
+module InputGraphQLTypes = 
+
+    let AppPreviewInputType = 
+        Define.InputObject<AppInputType>(
+            name = "AppPreviewInput",
+            fieldsFn = fun () ->
+            [
+                Define.Input("schemaName", String)
+                Define.Input("tableName", String)
+            ]
+        )
