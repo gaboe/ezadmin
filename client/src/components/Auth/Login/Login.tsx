e9df7cea645474a49f68d4da26188a3ab94e4c3a
gaboe <gabrielecegi@gmail.com>
2018-11-18 11:42:40 +0100

    Add formik and yup

--- client/src/components/Auth/Login/Login.tsx
+++ client/src/components/Auth/Login/Login.tsx
@@ -1,22 +1,66 @@
 import * as React from "react";
 
+import { Formik, FormikProps } from "formik";
 import { Col, Row } from "react-grid-system";
 import { Button, Form } from "semantic-ui-react";
+import * as Yup from "yup";
+type UserLogin = { email: string; password: string };
+const initialUser: UserLogin = { email: "", password: "" };
+
+const submitForm = (model: UserLogin) => {
+  console.log(model);
+};
+
+const LoginForm = (props: FormikProps<UserLogin>) => {
+  return (
+    <>
+      <Form>
+        <Form.Field error={props.errors.email !== undefined}>
+          <label>Email</label>
+          <input
+            value={props.values.email}
+            onChange={props.handleChange}
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+        </Form.Field>
+        <Form.Field error={props.errors.password !== undefined}>
+          <label>Password</label>
+          <input
+            value={props.values.password}
+            onChange={props.handleChange}
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+        </Form.Field>
+        <Button onClick={props.submitForm} type="submit">
+          Login
+        </Button>
+      </Form>
+    </>
+  );
+};
 
 const Login = () => (
   <Row>
-    <Col offset={{ lg: 3 }} lg={6}>
-      <Form>
-        <Form.Field>
-          <label>Email</label>
-          <input type="email" placeholder="email" />
-        </Form.Field>
-        <Form.Field>
-          <label>Password</label>
-          <input type="password" placeholder="password" />
-        </Form.Field>
-        <Button type="submit">Login</Button>
-      </Form>
+    <Col offset={{ lg: 4 }} lg={4}>
+      <Formik
+        initialValues={initialUser}
+        onSubmit={values => {
+          submitForm(values);
+        }}
+        render={LoginForm}
+        validationSchema={Yup.object<UserLogin>({
+          password: Yup.string()
+            .min(2, "Too Short!")
+            .required("Required"),
+          email: Yup.string()
+            .email("Invalid email")
+            .required("Required")
+        })}
+      />
     </Col>
   </Row>
 );
