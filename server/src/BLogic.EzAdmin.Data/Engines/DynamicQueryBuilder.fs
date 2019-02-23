8fb2f6a40f0232de77b43466781fe4eaade7947b
gaboe <gabrielecegi@gmail.com>
2019-02-23 20:40:47 +0100

    Refactor DynamicQueryBuilder

--- server/src/BLogic.EzAdmin.Data/Engines/DynamicQueryBuilder.fs
+++ server/src/BLogic.EzAdmin.Data/Engines/DynamicQueryBuilder.fs
@@ -7,61 +7,35 @@ open System.Text
 type SB = StringBuilder
 
 module DynamicQueryBuilder =
-    let private getColumnName (column: ColumnQueryDescription) = 
-        column.Column.ColumnName
+    let private appendLines (sb: SB) lines = lines |> Seq.iter (fun l -> sb.AppendLine l |> ignore)
 
-    let private getColumnNameWithAlias (column: ColumnQueryDescription) = 
-        column.TableAlias + "." + column.Column.ColumnName
-
-    let private appendColumn columnName (sb: StringBuilder) = 
-        sb.Append(",") |> ignore
-        sb.AppendLine(columnName) |> ignore
+    let private appendFrom table (sb: SB) =
+        sprintf "FROM %s.%s AS %s" table.SchemaName table.TableName table.TableAlias 
+            |> sb.AppendLine |> ignore
     
-    let private appendFrom table (sb: StringBuilder) =
-        sb.Append("FROM ") |> ignore
-        sb.Append(table.SchemaName) |> ignore
-        sb.Append(".") |> ignore
-        sb.Append(table.TableName) |> ignore
-        sb.Append(" AS ") |> ignore
-        sb.AppendLine(table.TableAlias) |> ignore
+    let private getPrimaryKey table =
+        table.Columns |> Seq.find (fun e -> e.Column.ColumnType = ColumnType.PrimaryKey)
     
-    let private isPrimaryKey column =
-        column.Column.ColumnType = ColumnType.PrimaryKey
-
-    let private isNotPrimaryKey column =
-        isPrimaryKey column |> not
-    
-    let rec flattenColumns col = 
-        match col.Column.Reference with 
-            | Some r ->  flattenColumns {TableAlias= col.TableAlias; Column = r} 
-                        |> Seq.collect (fun c -> [c])
-                        |> Seq.append [col]
-                        |> Seq.toList
-            | Option.None -> [col] |> Seq.toList
-
-    let private getPrimaryTableMainKey table =
-        table.Columns
-            |> Seq.map flattenColumns
-            |> Seq.collect (fun e -> e)
-            |> Seq.find isPrimaryKey
-
-    let private appendJoin 
-                    (sb: StringBuilder)
-                    (resolveAlias: _ -> string)
-                    (col: ColumnQueryDescription) =
+    let private resolveAlias alltables (schemaName, tableName) =
+        let table = alltables
+                    |> Seq.find (fun e -> e.SchemaName = schemaName
+                                        && e.TableName = tableName)
+        table.TableAlias  
+        
+    let private appendJoin alltables col =
 
         let (schema, table, column) = match col.Column.Reference with
                                         | Some e -> (e.SchemaName, e.TableName, e.ColumnName)
                                         | Option.None -> ("","","")
-        let alias = (schema, table) |> resolveAlias
+        let alias = (schema, table) |> resolveAlias alltables
 
         let join = sprintf "JOIN %s.%s %s " col.Column.SchemaName col.Column.TableName col.TableAlias
                     + sprintf "ON "
                     + sprintf "%s.%s = %s.%s" alias column col.TableAlias col.Column.ColumnName 
 
-        join |> sb.AppendLine |> ignore 
+        join 
 
-    let private appendJoins (foreignTables: TableQueryDescription list) (sb: StringBuilder) resolveAlias = 
+    let private appendJoins (foreignTables: TableQueryDescription list) (sb: SB) allTables = 
         let foreignColumns = foreignTables 
                             |> Seq.collect
                                 (fun e -> e.Columns 
@@ -71,7 +45,8 @@ module DynamicQueryBuilder =
                             |> Seq.distinct
                             |> Seq.toList
 
-        foreignColumns |> Seq.iter (fun e -> appendJoin sb resolveAlias e)
+        let joins = foreignColumns |> Seq.map (fun e -> appendJoin allTables e)
+        appendLines sb joins 
     
     let private getColumnsFromTable tables filter map =
          tables
@@ -79,51 +54,44 @@ module DynamicQueryBuilder =
                                     |> Seq.filter filter
                                     |> Seq.map map)
             |> Seq.toList
+    
+    let private columnVisibiltyFilter col =
+        not (col.Column.ColumnType = ColumnType.PrimaryKey) && not col.Column.IsHidden
 
     let private getColumnNamesExeptPrimary (tables: TableQueryDescription list) = 
-        let filter col = isNotPrimaryKey col && not col.Column.IsHidden
-        getColumnsFromTable tables filter (fun c -> c.Column.ColumnName)
+        getColumnsFromTable tables columnVisibiltyFilter (fun c -> c.Column.ColumnName)
 
     let private getColumnNamesWithAliasesExeptPrimary (tables: TableQueryDescription list) = 
-        let filter col = isNotPrimaryKey col && not col.Column.IsHidden
-        getColumnsFromTable tables filter (fun c -> c.TableAlias + "." + c.Column.ColumnName)
+        getColumnsFromTable tables columnVisibiltyFilter (fun c -> c.TableAlias + "." + c.Column.ColumnName)
 
     let private appendColumnNames (sb: SB) names =
-        names |> Seq.iter (fun n -> appendColumn n sb |> ignore)
+        names |> Seq.map (fun n -> sprintf ",%s" n) |> appendLines sb
 
-    let private resolveAlias tables (schemaName, tableName) =
-        let table = tables
-                    |> Seq.find (fun e -> e.SchemaName = schemaName
-                                        && e.TableName = tableName)
-        table.TableAlias                                        
+    let private mainTablePrimaryKey description = 
+        description.MainTable 
+            |> getPrimaryKey
+            |> fun column -> sprintf "%s.%s" column.TableAlias column.Column.ColumnName
 
     let buildQuery (description: QueryDescription) = 
-        let sb = StringBuilder()
-        "SELECT " |> sb.AppendLine |> ignore
-        
-        description.MainTable 
-            |> getPrimaryTableMainKey
-            |> getColumnNameWithAlias
-            |> sb.AppendLine
-            |> ignore
         let allTables = description.MainTable :: description.JoinedTables
-
         let columnNames = allTables |> getColumnNamesWithAliasesExeptPrimary
+
+        let sb = SB()
+        (sprintf "SELECT %s" (mainTablePrimaryKey description)) |> sb.AppendLine |> ignore
 
         appendColumnNames sb columnNames
 
         appendFrom description.MainTable sb
 
-        let resAlias = resolveAlias allTables
-        appendJoins description.JoinedTables sb resAlias
+        appendJoins description.JoinedTables sb allTables
 
         sb.ToString()
 
     let getHeaders description =
         let tables = (description.MainTable :: description.JoinedTables) 
         let mainKey = description.MainTable
-                        |> getPrimaryTableMainKey 
-                        |> getColumnName
+                        |> getPrimaryKey 
+                        |> (fun e -> e.Column.ColumnName)
 
         {
          KeyName = mainKey;
