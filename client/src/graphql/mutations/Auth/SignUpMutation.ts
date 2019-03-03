97aa9af49b56d77c0e4705ab9fdafd6fa1116f1b
Gabriel <gabriel.ecegi@blogic.cz>
2019-03-03 15:36:29 +0100

    Add signup mutation

---
+++ client/src/graphql/mutations/Auth/SignUpMutation.ts
@@ -0,0 +1,15 @@
+import gql from 'graphql-tag';
+// import { Mutation } from 'react-apollo';
+// import { SignUpMutation, SignUpMutationVariables } from 'src/domain/generated/types';
+
+const SIGN_UP_MUTATION = gql`
+  mutation SignUpMutation($email: String!, $password: String!) {
+    signup(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+// class SignUpMutationComponent extends Mutation<SignUpMutation, SignUpMutationVariables>{ }
+
+export { SIGN_UP_MUTATION };
