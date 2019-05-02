59f3a717664d7073780dead23fc3f1733cd27714
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2019-05-02 21:30:24 +0200

    Add footer

--- client/src/components/Auth/Logout/Logout.tsx
+++ client/src/components/Auth/Logout/Logout.tsx
@@ -1,16 +1,22 @@
-import * as React from 'react';
-import { Icon } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { AUTHORIZATION_TOKEN } from "../../../domain/Constants";
 
 const Logout: React.FunctionComponent = _ => {
-    return (<>
-        <Link to="/login" onClick={() => {
-            localStorage.removeItem("AUTHORIZATION_TOKEN");
-        }} >
-            <Icon name="sign-out" />
-            <>Sign out</>
-        </Link>
-    </>)
-}
+	return (
+		<>
+			<Link
+				to="/login"
+				onClick={() => {
+					localStorage.removeItem(AUTHORIZATION_TOKEN);
+				}}
+			>
+				<Icon name="sign-out" />
+				<>Sign out</>
+			</Link>
+		</>
+	);
+};
 
 export { Logout };
--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,47 +1,61 @@
 import * as React from "react";
 import styled from "styled-components";
-import { APPID_QUERY, AppIDQueryComponent } from "../../graphql/queries/Auth/AppIDQuery";
-import { Menu, Segment, Sidebar } from "semantic-ui-react";
+import {
+	APPID_QUERY,
+	AppIDQueryComponent
+} from "../../graphql/queries/Auth/AppIDQuery";
+import { Menu, Segment, Sidebar, Container, Icon } from "semantic-ui-react";
 import { MenuItems } from "./MenuItems";
 
 const Pushable = styled.div`
-  min-height: calc(100vh);
+	min-height: calc(95vh);
 `;
 
 const Children = styled.div`
-  margin-top: 4em;
+	margin-top: 4em;
+`;
+
+const StyledSegment = styled(Segment)`
+	padding: 1em 0em;
 `;
 
 const Layout: React.FunctionComponent = props => {
-  return (
-    <Sidebar.Pushable as={Segment}>
-      <Pushable>
-        <Sidebar
-          as={Menu}
-          animation="overlay"
-          direction={"top"}
-          visible={true}
-          inverted={true}
-        >
-          <AppIDQueryComponent query={APPID_QUERY} fetchPolicy="cache-first">
-            {response => {
-              if (response.data) {
-                return (
-                  <MenuItems app={response.data.currentApp} />
-                );
-              }
-              return null;
-            }}
-          </AppIDQueryComponent>
-        </Sidebar>
-        <Sidebar.Pusher>
-          <Segment basic={true}>
-            <Children>{props.children}</Children>
-          </Segment>
-        </Sidebar.Pusher>
-      </Pushable>
-    </Sidebar.Pushable>
-  );
-}
+	return (
+		<Sidebar.Pushable as={Segment}>
+			<Pushable>
+				<Sidebar
+					as={Menu}
+					animation="overlay"
+					direction={"top"}
+					visible={true}
+					inverted={true}
+				>
+					<AppIDQueryComponent query={APPID_QUERY} fetchPolicy="cache-first">
+						{response => {
+							if (response.data) {
+								return <MenuItems app={response.data.currentApp} />;
+							}
+							return null;
+						}}
+					</AppIDQueryComponent>
+				</Sidebar>
+				<Sidebar.Pusher>
+					<Segment basic={true}>
+						<Children>{props.children}</Children>
+					</Segment>
+				</Sidebar.Pusher>
+			</Pushable>
+			<StyledSegment inverted vertical>
+				<Container textAlign="center">
+					<div>
+						<span>Developed by Gabriel Ečegi with &nbsp;</span>
+						<Icon name="heart" />
+						<span>2019</span>
+					</div>
+				</Container>
+			</StyledSegment>
+		</Sidebar.Pushable>
+	);
+};
 
 export { Layout };
